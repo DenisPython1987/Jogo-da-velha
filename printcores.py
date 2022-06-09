3ec05b83ba24c197be68b6ad3d0132c93b1b8e82
DenisPython1987 <105618584+DenisPython1987@users.noreply.github.com>
2022-06-09 14:22:13 -0300

    Add files via upload

---
+++ printcores.py
@@ -0,0 +1,22 @@
+def vermelho(msg):
+    print(f'\033[1;31m{msg}\033[m')
+
+
+def verde(msg):
+    print(f'\033[1;32m{msg}\033[m')
+
+
+def amarelo(msg):
+    print(f'\033[1;33m{msg}\033[m')
+
+
+def azul(msg):
+    print(f'\033[1;34m{msg}\033[m')
+
+
+def roxo(msg):
+    print(f'\033[1;35m{msg}\033[m')
+
+
+def turquesa(msg):
+    print(f'\033[1;36m{msg}\033[m')
